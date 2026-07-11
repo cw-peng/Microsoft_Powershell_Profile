cac7b33cca1948997e6b32b584848636b73396c7
cw-peng <cwp0302@outlook.com>
2026-07-11 14:38:29 +0800

    Refactor PowerShell profile with terminal icons and PSReadLine

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,191 +1,144 @@
-"$schema" = 'https://starship.rs/config-schema.json'
+# --------------Terminal-Icons------------
+$script:TerminalIconsLoaded = $false
 
-format = """
-[](color_orange)\
-$os\
-$username\
-[](bg:color_yellow fg:color_orange)\
-$directory\
-[](fg:color_yellow bg:color_aqua)\
-$git_branch\
-$git_status\
-[](fg:color_aqua bg:color_blue)\
-$c\
-$cpp\
-$rust\
-$golang\
-$nodejs\
-$bun\
-$php\
-$java\
-$kotlin\
-$haskell\
-$python\
-[](fg:color_blue bg:color_bg3)\
-$docker_context\
-$conda\
-$pixi\
-[](fg:color_bg3 bg:color_bg1)\
-$time\
-[ ](fg:color_bg1)\
-$line_break$cmd_duration$character"""
+function global:Ensure-TerminalIcons {
+    if (-not $script:TerminalIconsLoaded) {
+        Import-Module Terminal-Icons
+        $script:TerminalIconsLoaded = $true
+    }
+}
 
-palette = 'gruvbox_dark'
+# Register-EngineEvent PowerShell.OnIdle `
+#     -MaxTriggerCount 1 `
+#     -Action { Ensure-TerminalIcons } | Out-Null
+# ----------------------------------------
 
-[palettes.gruvbox_dark]
-color_fg0 = '#fbf1c7'
-color_bg1 = '#3c3836'
-color_bg3 = '#665c54'
-color_blue = '#458588'
-color_aqua = '#689d6a'
-color_green = '#98971a'
-color_orange = '#d65d0e'
-color_purple = '#b16286'
-color_red = '#cc241d'
-color_yellow = '#d79921'
+# ---------------PSReadLine-------------
+$script:PSReadLineSet = $false
 
-[os]
-disabled = false
-style = "bg:color_orange fg:color_fg0"
+function global:Ensure-PSReadLine {
+    if (-not $script:PSReadLineSet) {
+        $script:PSReadLineSet = $true
+        Set-PSReadLineOption `
+        -PredictionSource History `
+        -PredictionViewStyle InlineView `
+        -HistorySearchCursorMovesToEnd `
+        -MaximumHistoryCount 500
 
-[os.symbols]
-Windows = "󰍲"
-Ubuntu = "󰕈"
-SUSE = ""
-Raspbian = "󰐿"
-Mint = "󰣭"
-Macos = "󰀵"
-Manjaro = ""
-Linux = "󰌽"
-Gentoo = "󰣨"
-Fedora = "󰣛"
-Alpine = ""
-Amazon = ""
-Android = ""
-AOSC = ""
-Arch = "󰣇"
-Artix = "󰣇"
-EndeavourOS = ""
-CentOS = ""
-Debian = "󰣚"
-Redhat = "󱄛"
-RedHatEnterprise = "󱄛"
-Pop = ""
+        # 设置 Tab 为菜单补全和 Intellisense
+        Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete 
+        
+        # 设置 Ctrl+d 为退出 PowerShell
+        Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+    }
+}
+# Register-EngineEvent PowerShell.OnIdle `
+#     -MaxTriggerCount 1 `
+#     -Action { Ensure-PSReadLine} | Out-Null
+# -----------------------------------------
 
-[username]
-show_always = true
-style_user = "bg:color_orange fg:color_fg0"
-style_root = "bg:color_orange fg:color_fg0"
-format = '[ $user ]($style)'
+# ------------PSFzf------------------------
+$script:PSFzfLoaded = $false
 
-[directory]
-style = "fg:color_fg0 bg:color_yellow"
-format = "[ $path ]($style)"
-truncation_length = 3
-truncation_symbol = "…/"
+function global:Ensure-PSFzf {
+    if (-not $script:PSFzfLoaded) {
+        Import-Module PSFzf
+        Set-PsFzfOption -PSReadlineChordReverseHistory Ctrl+r
+        $script:PSFzfLoaded = $true
+    }
+}
 
-[directory.substitutions]
-"Documents" = "󰈙 "
-"Downloads" = " "
-"Music" = "󰝚 "
-"Pictures" = " "
-"Developer" = "󰲋 "
+# Register-EngineEvent PowerShell.OnIdle `
+#     -MaxTriggerCount 1 `
+#     -Action { Ensure-PSFzf} | Out-Null
+# ------------------------------------------
 
-[git_branch]
-symbol = ""
-style = "bg:color_aqua"
-format = '[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)'
+# -------------zoxide----------------------
+$script:ZoxideLoaded = $false
 
-[git_status]
-style = "bg:color_aqua"
-format = '[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)'
+function global:Ensure-Zoxide {
+    if (-not $script:ZoxideLoaded) {
+        Invoke-Expression (& { (zoxide init powershell | Out-String) })
+        $script:ZoxideLoaded = $true
+    }
+}
 
-[nodejs]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# Register-EngineEvent PowerShell.OnIdle `
+#     -MaxTriggerCount 1 `
+#     -Action { Ensure-Zoxide } | Out-Null
+# -----------------------------------------
 
-[bun]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# ----------Register----------------------
+$global:IdleTasks = @(
+    {Ensure-TerminalIcons} 
+    {Ensure-PSFzf} 
+    {Ensure-PSReadLine} 
+    {Ensure-Zoxide}
+)
 
-[c]
-symbol = " "
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+Register-EngineEvent PowerShell.OnIdle `
+    -MaxTriggerCount 1 `
+    -Action {
+        foreach ($task in $global:IdleTasks) {
+            & $task
+        }
+    } | Out-Null
+# -----------------------------------------
 
-[cpp]
-symbol = " "
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# --------------alias----------------------
+# 使用 ll 查看目录
+function ll {
+    Get-ChildItem
+}
 
-[rust]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# touch alias
+function touch {
+    param([string]$Path)
 
-[golang]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+    try {
+        (Get-Item -LiteralPath $Path).LastWriteTime = Get-Date
+    }
+    catch {
+        New-Item -ItemType File -Path $Path | Out-Null
+    }
+}
+# ----------------------------------------------
 
-[php]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# zoxide
+# Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-[java]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# -------------- starship config ----------------
+$ENV:STARSHIP_CONFIG = "C:\Users\cwp\.config\starship.toml"
+function Invoke-Starship-TransientFunction {
+  &starship module character
+}
+Invoke-Expression (&starship init powershell)
 
-[kotlin]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+Enable-TransientPrompt
 
-[haskell]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)'
+# # -----------------------------------------------
+# #region vscode python
+# #version: 0.1.1
 
-[python]
-symbol = ""
-style = "bg:color_blue"
-format = '[[ $symbol( $version $virtualenv) ](fg:color_fg0 bg:color_blue)]($style)'
+# if (-not $env:VSCODE_PYTHON_AUTOACTIVATE_GUARD) {
 
-[docker_context]
-symbol = ""
-style = "bg:color_bg3"
-format = '[[ $symbol( $context) ](fg:#83a598 bg:color_bg3)]($style)'
+#     $env:VSCODE_PYTHON_AUTOACTIVATE_GUARD = '1'
 
-[conda]
-style = "bg:color_bg3"
-format = '[[ $symbol( $environment) ](fg:#83a598 bg:color_bg3)]($style)'
+#     if (($env:TERM_PROGRAM -eq 'vscode') -and ($null -ne $env:VSCODE_PYTHON_PWSH_ACTIVATE)) {
 
-[pixi]
-style = "bg:color_bg3"
-format = '[[ $symbol( $version)( $environment) ](fg:color_fg0 bg:color_bg3)]($style)'
+#         try {
 
-[time]
-disabled = false
-time_format = "%R"
-style = "bg:color_bg1"
-format = '[[  $time ](fg:color_fg0 bg:color_bg1)]($style)'
+#             Invoke-Expression $env:VSCODE_PYTHON_PWSH_ACTIVATE
 
-[line_break]
-disabled = false
+#         } catch {
 
-[character]
-disabled = false
-success_symbol = '[](bold fg:color_green)'
-error_symbol = '[](bold fg:color_red)'
-vimcmd_symbol = '[](bold fg:color_green)'
-vimcmd_replace_one_symbol = '[](bold fg:color_purple)'
-vimcmd_replace_symbol = '[](bold fg:color_purple)'
-vimcmd_visual_symbol = '[](bold fg:color_yellow)'
+#             $psVersion = $PSVersionTable.PSVersion.Major
 
-[cmd_duration]
-min_time = 500
-format = ' [$duration](color_green) '
+#             Write-Error "Failed to activate Python environment (PowerShell $psVersion): $_" -ErrorAction Continue
+
+#         }
+
+#     }
+
+# }
+# #endregion vscode python
